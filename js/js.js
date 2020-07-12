2f1179b236c0637b2ac8baa41ab4db4607a74055
Torza1997 <60089917+Torza1997@users.noreply.github.com>
2020-07-13 04:34:21 +0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 0572e1cce3a084651ede44911d307e6ff08d172c
 function toggle(){
     var head = document.getElementById("header");
     head.classList.toggle('active');
@@ -13,14 +9,11 @@ VanillaTilt.init(document.querySelector(".img_box"), {
 });
 
 //timeline 3d
-<<<<<<< HEAD
 VanillaTilt.init(document.querySelector(".Timeline"), {
     max: 25,
     speed: 400
 });
 
-=======
->>>>>>> 0572e1cce3a084651ede44911d307e6ff08d172c
 var w = window.innerWidth;
 if(w > 777){
   VanillaTilt.init(document.querySelector(".Timeline"), {
@@ -30,10 +23,6 @@ if(w > 777){
    }else{
     //not do
    }
-<<<<<<< HEAD
-
-=======
->>>>>>> 0572e1cce3a084651ede44911d307e6ff08d172c
 //skill 3d 
 
 
